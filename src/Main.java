1b10c2493352a58dbf0c1cfba7a144fce7ad6e22
Bratan Radu <hyperbeast.mlgboss@gmail.com>
2023-04-07 22:27:27 +0300

    Updated commands

--- src/Main.java
+++ src/Main.java
@@ -2,33 +2,33 @@ import java.util.*;
 
 public class Main {
 
-    static List<String> commandNos = Arrays.asList(
+    static List<String> commandNumbers = Arrays.asList(
             "1",
-            "create_developer_account",
+            // "create_developer_account",
             "2",
-            "create_user_account",
+            // "create_user_account",
             "3",
-            "show_developer_account",
+            // "show_developer_account",
             "4",
-            "show_user_account",
+            // "show_user_account",
             "5",
-            "add_game_to_dev",
+            // "add_game_to_dev",
             "6",
-            "add_review_to_game",
+            // "add_review_to_game",
             "7",
-            "show_all_games_of_dev",
+            // "show_all_games_of_dev",
             "8",
-            "show_all_reviews_of_game",
+            // "show_all_reviews_of_game",
             "9",
-            "remove_offensive_game_of_dev",
+            // "remove_offensive_game_of_dev",
             "10",
-            "remove_offensive_review_of_game",
+            // "remove_offensive_review_of_game",
             "11",
-            "commands",
+            // "commands",
             "12",
-            "finish"
+            // "finish"
     );
-    static List<String> commandDescs = Arrays.asList(
+    static List<String> commandDescriptions = Arrays.asList(
             "Creează un cont pentru un dezvoltator",
             "Creează un cont pentru un utilizator",
             "Vizionează contul unui dezvoltator",
@@ -44,8 +44,8 @@ public class Main {
     );
 
     private static void showAllCommands() {
-        for (int i = 0; i < commandNos.size(); ++i)
-            System.out.println((i + 1) + ". " + commandDescs.get(i) + " (" + commandNos.get(i) + ")");
+        for (int i = 0; i < commandNumbers.size(); ++i)
+            System.out.println((i + 1) + ". " + commandDescriptions.get(i) + " (" + commandNumbers.get(i) + ")");
     }
 
     public static void main(String[] args) {
@@ -58,40 +58,40 @@ public class Main {
             try {
                 switch (command) {
                     case "1":
-                    case "create_developer_account":
+                    // case "create_developer_account":
                         new MainService().createDeveloper(in);
                     case "2":
-                    case "create_user_account":
+                    // case "create_user_account":
                         new MainService().createUser(in);
                     case "3":
-                    case "show_developer_account":
+                    // case "show_developer_account":
                         new MainService().showDeveloper(in);
                     case "4":
-                    case "show_user_account":
+                    // case "show_user_account":
                         new MainService().showUser(in);
                     case "5":
-                    case "add_game_to_dev":
+                    // case "add_game_to_dev":
                         new MainService().addGame(in);
                     case "6":
-                    case "add_review_to_game":
+                    // case "add_review_to_game":
                         new MainService().addReview(in);
                     case "7":
-                    case "show_all_games_of_dev":
+                    // case "show_all_games_of_dev":
                         new MainService().showGames(in);
                     case "8":
-                    case "show_all_reviews_of_game":
+                    // case "show_all_reviews_of_game":
                         new MainService().showReviews(in);
                     case "9":
-                    case "remove_offensive_game_of_dev":
+                    // case "remove_offensive_game_of_dev":
                         new MainService().removeGame(in);
                     case "10":
-                    case "remove_offensive_review_of_game":
+                    // case "remove_offensive_review_of_game":
                         new MainService().removeReview(in);
                     case "11":
-                    case "commands":
+                    // case "commands":
                         Main.showAllCommands();
                     case "12":
-                    case "finish":
+                    // case "finish":
                         end = true;
                 }
             } catch (Exception e) {
